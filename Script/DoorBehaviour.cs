d45ad4dc9e460a351ac97912dd165538847c5e7b
minnieie <s10265787@connect.np.edu.sg>
2025-06-14 16:16:33 +0800

    Final DoorBehaviour.cs

--- Script/DoorBehaviour.cs
+++ Script/DoorBehaviour.cs
@@ -2,27 +2,130 @@ using UnityEngine;
 
 public class DoorBehaviour : MonoBehaviour
 {
-    private bool isOpen = false; //tracks if the door is open or closed
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void interact()
+    AudioSource doorAudioSource;
+    public PanelToggle panelToggle; //UI popup reference
+
+    [Header("Door Settings")]
+    public float openAngle = 90f;
+    public float openSpeed = 2f;
+    public float closeDistance = 3f;
+    public Transform pivotPoint;
+
+    [Header("Lock Settings")]
+    public bool isLocked = false;
+    public AudioClip lockedSound;
+    public Material lockedMaterial; // Added missing reference
+
+    private Quaternion initialRotation;
+    private Quaternion targetRotation;
+    private Transform player;
+    private bool isOpen = false;
+    private bool isMoving = false;
+    private MeshRenderer doorRenderer;
+    private Material originalMaterial;
+
+    void Start()
     {
-        Vector3 doorRotation = transform.eulerAngles;
-        if (!isOpen)
+        doorAudioSource = GetComponent<AudioSource>();
+        doorRenderer = GetComponent<MeshRenderer>();
+        originalMaterial = doorRenderer.material;
+
+        initialRotation = transform.rotation;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if (pivotPoint == null) pivotPoint = transform;
+
+        // Visual feedback for locked doors
+        if (isLocked && lockedMaterial != null)
         {
-            doorRotation.y += 90f; //open
-            isOpen = true;
+            doorRenderer.material = lockedMaterial;
+        }
+    }
+
+    void Update()
+    {
+        // Smooth door rotation
+        if (Quaternion.Angle(transform.rotation, targetRotation) > 0.1f)
+        {
+            isMoving = true;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, openSpeed * Time.deltaTime);
         }
         else
         {
-            doorRotation.y -= 90f; //close
-            isOpen = false;
+            isMoving = false;
+            // Snap to target rotation when close enough
+            if (Quaternion.Angle(transform.rotation, targetRotation) > 0)
+            {
+                transform.rotation = targetRotation;
+            }
         }
-        transform.eulerAngles = doorRotation;
+
+        // Auto-close when player is far away
+        if (isOpen && !isMoving && Vector3.Distance(pivotPoint.position, player.position) > closeDistance)
+        {
+            CloseDoor();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerStay(Collider other)
     {
-        
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            TryInteractWithDoor(other.GetComponent<PlayerBehaviour>());
+        }
+    }
+
+    private void TryInteractWithDoor(PlayerBehaviour player)
+    {
+        if (isLocked)
+        {
+            // Check for key FIRST
+            if (player != null && player.hasCardKey)
+            {
+                UnlockDoor();
+                ToggleDoor();
+                return; 
+            }
+            else
+            {
+                if (lockedSound != null) doorAudioSource.PlayOneShot(lockedSound);
+                if (panelToggle != null) panelToggle.ShowMessage();
+            }
+        }
+        else
+        {
+            ToggleDoor();
+        }
+    }
+    public void ToggleDoor()
+    {
+        if (!isOpen)
+        {
+            if (doorAudioSource != null) doorAudioSource.Play();
+            OpenDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+
+    public void UnlockDoor()
+    {
+        isLocked = false;
+        if (doorRenderer != null) doorRenderer.material = originalMaterial;
+        Debug.Log("Door unlocked!");
+    }
+
+    void OpenDoor()
+    {
+        targetRotation = initialRotation * Quaternion.Euler(0, openAngle, 0);
+        isOpen = true;
+    }
+
+    void CloseDoor()
+    {
+        targetRotation = initialRotation;
+        isOpen = false;
     }
 }
